5c2aef8f1a252a37502c38037113d10d8081b03a
Illya Kovaliuk <>
2025-11-15 14:31:55 +0200

    Initial note

---
+++ WeatherNotesApp/WeatherNotesApp/WeatherNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesAppApp.swift
+//  WeatherNotesApp
+//
+//  Created by Illya Kovaliuk on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
