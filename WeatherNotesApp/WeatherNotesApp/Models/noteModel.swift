80a4770172d40e9d405a824fd790c5b894a3c729
Illya Kovaliuk <>
2025-11-16 02:36:34 +0200

    integrated new weather model in listScreen

--- WeatherNotesApp/WeatherNotesApp/Models/noteModel.swift
+++ WeatherNotesApp/WeatherNotesApp/Models/noteModel.swift
@@ -11,13 +11,23 @@ struct NoteModel: Identifiable {
     var id = UUID()
     var title: String
     var createTime: String
-    var weatherTemp: Double
-    var weatherIcon: String
+    var weather: weatherModel?
 }
 
+
+
 var testData = [
-    NoteModel(title: "Go for a walk", createTime: "15.11.25", weatherTemp: 20.0, weatherIcon: "circle.fill"),
-    NoteModel(title: "Cycling", createTime: "16.11.25", weatherTemp: 18.0, weatherIcon: "sun.min.fill"),
-    NoteModel(title: "Runing club", createTime: "17.11.25", weatherTemp: 10.0, weatherIcon: "sun.haze"),
-    NoteModel(title: "Phone to mother from work", createTime: "15.11.25", weatherTemp: 24.0, weatherIcon: "moon.fill")
+    NoteModel(title: "Go riding", createTime: "15.11.25", weather: weatherTest[0]),
+    NoteModel(title: "Go cycling", createTime: "15.11.25", weather: weatherTest[1]),
+    NoteModel(title: "Write test", createTime: "15.11.25", weather: weatherTest[2])
+
 ]
+
+
+
+//var testData = [
+//    NoteModel(title: "Go for a walk", createTime: "15.11.25", weatherTemp: 20.0, weatherIcon: "circle.fill"),
+//    NoteModel(title: "Cycling", createTime: "16.11.25", weatherTemp: 18.0, weatherIcon: "sun.min.fill"),
+//    NoteModel(title: "Runing club", createTime: "17.11.25", weatherTemp: 10.0, weatherIcon: "sun.haze"),
+//    NoteModel(title: "Phone to mother from work", createTime: "15.11.25", weatherTemp: 24.0, weatherIcon: "moon.fill")
+//]
--- WeatherNotesApp/WeatherNotesApp/Screens/listScreenGroup/listScreen.swift
+++ WeatherNotesApp/WeatherNotesApp/Screens/listScreenGroup/listScreen.swift
@@ -15,7 +15,6 @@ struct listView: View {
             List{
                 ForEach(viewModel.list){ note in
                     noteUnit(note: note)
-                    
                 }
             }
             .navigationTitle("Notes list")
@@ -34,14 +33,24 @@ struct noteUnit: View {
     var body: some View {
         NavigationLink(destination: detailNoteScreen(note: note)){
             HStack{
-                Image(note.weatherIcon)
-                VStack{
+                if let weather = note.weather{
+                    Image(systemName: weather.icon)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 50, height: 30)
+                }
+                VStack(alignment: .leading){
                     Text(note.title)
+                        .font(.headline)
+                        .foregroundStyle(.primary)
                     Text(note.createTime)
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
                 }
                 Spacer()
-                
-                Text(String(note.weatherTemp))
+                if let weather = note.weather{
+                    Text("\(weather.temperature, specifier: "%.1f")°")
+                }
             }
         }
     }
